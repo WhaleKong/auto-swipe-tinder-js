d1d595a8f3811250acb40530430db1a5695615e7
WhaleKong <52395934+WhaleKong@users.noreply.github.com>
2020-09-27 00:24:15 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const swipTinderJaJa = () => {
+  const x = setInterval(() => {
+    const closePopupMatch = document.getElementsByClassName(
+      "Pt(20px) Pb(40px) C(#fff) Fw($bold) Tt(u) Lts($ls-s) D(b) Cur(p) W(100%) Trsdu($slow)"
+    );
+    const likeBtn = document.querySelector('[aria-label="Like"]');
+
+    if (!closePopupMatch.length) {
+      likeBtn.click();
+    } else {
+      closePopupMatch[0].click();
+    }
+  }, 5000);
+  setTimeout(() => {
+    clearInterval(x);
+  }, 20000);
+};
